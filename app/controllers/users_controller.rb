93d9bfcf807987a451cc50387e8a53cbca75c649
Gardimy Charles <gardimycharles889@gmail.com>
2023-10-02 18:45:25 -0400

    Add Action in the controllers file

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,23 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!, except: [:new, :create] # Allow non-authenticated users to access registration
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      sign_in(@user) # Automatically log in the user after registration
+      redirect_to root_path, notice: 'Welcome! You have successfully signed up.'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
 end
