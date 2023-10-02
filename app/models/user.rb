c5b7fe93a40a7577b6750ea55330511d0fda6e8a
Gardimy Charles <gardimycharles889@gmail.com>
2023-10-02 19:14:32 -0400

    Setup devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-  has_secure_password
+         :recoverable, :rememberable, :validatable, :confirmable
 
   has_many :transactions, foreign_key: :author_id
 end
