0a6ff1d0576034a2fefe08a84806c50029d30147
Gardimy Charles <gardimycharles889@gmail.com>
2023-10-05 10:55:40 -0400

    Update category_spec file

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,14 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
+  it "is valid with a name and icon" do
+    user = FactoryBot.create(:user) # Create a user using FactoryBot
+    category = Category.new(name: "Groceries", icon: "grocery-icon", user: user)
+    expect(category).to be_valid
+  end
 
-  it 'is not valid without a name' do
-    category = Category.new(icon: 'icon.png')
+  it "is not valid without a name" do
+    user = FactoryBot.create(:user) # Create a user using FactoryBot
+    category = Category.new(icon: "grocery-icon", user: user)
     expect(category).to_not be_valid
   end
 
-  it 'is not valid without an icon' do
-    category = Category.new(name: 'Example')
+  it "is not valid without an icon" do
+    user = FactoryBot.create(:user) # Create a user using FactoryBot
+    category = Category.new(name: "Groceries", user: user)
     expect(category).to_not be_valid
   end
 end
