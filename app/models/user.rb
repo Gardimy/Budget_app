a8c7ee2ad11fafb3058c5f98848df7897b70e6d0
Gardimy Charles <gardimycharles889@gmail.com>
2023-10-07 21:43:51 -0400

    Fix style according the design

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
   has_many :categories
   has_many :transactions, foreign_key: :author_id
+
+  attr_accessor :full_name
 end
